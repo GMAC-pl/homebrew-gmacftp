70b485a88021a307a4b68eea78932f18e940e0bb
GregoryMc86 <44270263+GregoryMc86@users.noreply.github.com>
2026-07-01 20:53:02 +0200

    Add gmacFTP 0.0.14 cask

---
+++ Casks/gmacftp.rb
@@ -0,0 +1,25 @@
+cask "gmacftp" do
+  version "0.0.14"
+  sha256 "a386fa85fbd3afdbeb3bd9f9c65bdf3524d097e76da1a8629996684d64281f5d"
+
+  url "https://github.com/GMAC-pl/gmacFTP/releases/download/v#{version}/gmacFTP-#{version}.dmg",
+      verified: "github.com/GMAC-pl/gmacFTP/"
+  name "gmacFTP"
+  desc "Dual-pane FTP/FTPS/SFTP client"
+  homepage "https://github.com/GMAC-pl/gmacFTP"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :big_sur
+
+  app "gmacFTP.app"
+
+  zap trash: [
+    "~/Library/Application Support/mackftp/client",
+    "~/Library/Saved Application State/app.mackftp.client.savedState",
+    "~/Library/Mobile Documents/com~apple~CloudDocs/gmacFTP",
+  ]
+end
